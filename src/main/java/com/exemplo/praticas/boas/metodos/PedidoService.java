ded04d8a3f68ba062b55210d3a6cc003c41151a8
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2021-02-08 17:35:14 -0300

    Ajustado exemplo boas práticas em relação a métodos

--- src/main/java/com/exemplo/praticas/boas/metodos/PedidoService.java
+++ src/main/java/com/exemplo/praticas/boas/metodos/PedidoService.java
@@ -4,16 +4,12 @@ package com.exemplo.praticas.boas.metodos;
  * Cada método tem uma única responsabilidade.
  * Poucas condições aninhadas.
  * Método pequenos.
- * Poucos parâmetros.
+ * Poucos parâmetros nos métodos.
  * Sem flags como parâmetros.
  */
 public class PedidoService {
 
 	private Double calculaDescontoPorQuantidade(final Integer quantidade) {
-		if (quantidade <= 0) {
-			throw new ValidationException("Quantidade deve ser maior que zero");
-		}
-
 		if (quantidade >= 100) {
 			return 20D;
 		} else if (quantidade >= 50) {
@@ -26,10 +22,6 @@ public class PedidoService {
 	}
 
 	private Double calculaDescontoPorEstoque(final Integer estoque) {
-		if (estoque <= 0) {
-			throw new ValidationException("Estoque deve ser maior que zero");
-		}
-
 		if (estoque > 1000) {
 			return +5D;
 		} else {
@@ -38,13 +30,6 @@ public class PedidoService {
 	}
 
 	private Double calculaDescontoSobreValor(final Double valor, final Double desconto) {
-		if (valor <= 0) {
-			throw new ValidationException("Valor do produto deve ser maior que zero");
-		}
-		if (desconto < 0) {
-			throw new ValidationException("Desconto deve maior ou igual a zero");
-		}
-
 		return valor * (1 + (desconto / 100));
 	}
 
--- src/main/java/com/exemplo/praticas/ruins/metodos/PedidoService.java
+++ src/main/java/com/exemplo/praticas/ruins/metodos/PedidoService.java
@@ -7,14 +7,11 @@ public class PedidoService {
 	 * Muitos parâmetros.
 	 * Possui flags como parâmetros.
 	 * Realiza mais de uma ação: determina qual o desconto e aplica o desconto ao valor. 
-	 * Executa as validações no meio do código
 	 */
 	public Double calcularDesconto(final Double valor, final Integer quantidade, final Integer estoque, final boolean descontoPorEstoque, final boolean descontoPorQuantidade) {
 		Double desconto = 0D;
 		if (descontoPorQuantidade) {
-			if (quantidade <= 0) {
-				throw new ValidationException("A quantidade deve ser maior que zero");
-			}
+
 			if (quantidade >= 100) {
 				desconto = 20D;
 			} else if (quantidade >= 50) {
@@ -24,18 +21,8 @@ public class PedidoService {
 			}
 		}
 
-		if (descontoPorEstoque) {
-			if (estoque <= 0) {
-				throw new ValidationException("O estoque deve ser maior que zero");
-			}
-
-			if (estoque > 1000) {
-				desconto = +5D;
-			}
-		}
-
-		if (valor <= 0) {
-			throw new ValidationException("O valor do produto deve ser maior que zero");
+		if (descontoPorEstoque && (estoque > 1000)) {
+			desconto = +5D;
 		}
 
 		return valor * (1 + (desconto / 100));
